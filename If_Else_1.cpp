e899b61d46983b963f28fa2c158e68dc2d43940e
unknown <smnilaypawale@gmail.com>
2024-04-16 23:29:02 +0530

    iacsd

---
+++ If_Else_1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the Number\n");
+	scanf("%d",&a);
+	
+	if(a%2==0)
+	{
+		printf("The Number is Even\n");
+	}
+	else
+	{
+		printf("The Number is Odd\n");
+	}
+}
